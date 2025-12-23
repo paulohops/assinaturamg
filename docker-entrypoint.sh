c25c6d4c9c6f09211f31c50ea22e2a2cd1a2f738
Paulonho <78318030+paulohops@users.noreply.github.com>
2025-12-22 23:30:30 -0300

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Substitui a porta do Apache com a variável $PORT
-sed -i "s/80/${PORT}/g" /etc/apache2/ports.conf /etc/apache2/sites-available/000-default.conf
-
-# Inicia o Apache
-apache2-foreground
